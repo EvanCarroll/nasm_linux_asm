b62dc7b220efa7d47fce5e736fb25c680719ca8e
Evan Carroll <me@evancarroll.com>
2018-09-29 19:50:42 -0500

    fixed style, added notes

--- system-calls/2_sysenter.asm
+++ system-calls/2_sysenter.asm
@@ -1,16 +1,29 @@
-BITS 32
-
-SECTION .text
+BITS 32 ; NOTE: sysenter only available in 32bit mode
 
 GLOBAL _start
 
-_start:
-  mov ax, 10
-  add al, 9
-	aaa
-	mov ebx, eax;
+SECTION .data
 
-	push done
+	helloWorld: db 'Hello world!',10
+
+SECTION .text
+
+_start:
+
+	push d0             ; with sysenter, we need return loc
+	push ecx
+	push edx
+	push ebp
+	mov ebp, esp
+
+	mov eax, 4          ; system call number (sys_write)
+	mov ebx, 1          ; file descriptor (stdout)
+	mov ecx, helloWorld ; message to write
+	mov edx, 13         ; message length
+	sysenter            ; call kernel
+d0:
+
+	push d1
 	push ecx
 	push edx
 	push ebp
@@ -19,5 +32,4 @@ _start:
 	mov  eax, 1  ; exit with error code 0
 	mov  ebx, 0  ; 'exit' system call
 	sysenter
-	done:
-
+d1:
--- system-calls/README.md
+++ system-calls/README.md
@@ -3,8 +3,8 @@ NASM and Linux x86 System Calls
 
 Researching system calls I soon found out there were three ways to make them.
 
-* `int 0x80`
+* **i386 Linux kernel ABI** `int 0x80`
 * `sysenter` (32-bit mode only)
-* `syscall`
+* **x86_64 Linux Kernel ABI** `syscall`
 
 In this directory I demonstrate the form all of these take
