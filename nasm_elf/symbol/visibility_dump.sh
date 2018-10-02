473f949652bb9227a5106acb5a5b8c65da5a3eb5
Evan Carroll <me@evancarroll.com>
2018-10-01 22:33:32 -0500

    added link to question on re.se

--- nasm_elf/symbol/visibility_dump.sh
+++ nasm_elf/symbol/visibility_dump.sh
@@ -8,5 +8,7 @@ nm "$FILE"
 printf "\n\n===readelf --symbols==="
 readelf --symbols "$FILE"
 
+# question asked about showing visibility w/ radare
+# https://reverseengineering.stackexchange.com/q/19507/22669
 printf "\n\n===radare2 -qc 'fs symbols; f'===\n"
 radare2 -qc 'fs symbols; f' "$FILE"
